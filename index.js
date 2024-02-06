868690fa4fd539e5dbe7d15d5c1d90e8bc9bb00f
Sailwayfun <liaoleon000513@gmail.com>
2024-02-06 11:32:12 +0800

    multiple Vue instances

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ Vue.createApp({
         };
     }
 }).mount("#app");
+
+// Vue.createApp({
+//     data() {
+//         return {
+//             firstName: "Garnt",
+//             lastName: "Foo"
+//         };
+//     }
+// }).mount("#app2");
