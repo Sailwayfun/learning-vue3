cbad05ae4d3bc535c467720d0e2726c94797277f
Sailwayfun <liaoleon000513@gmail.com>
2024-02-07 23:07:04 +0800

    using computed property to improve performance

--- section-2-18-STARTER/app.js
+++ section-2-18-STARTER/app.js
@@ -1,5 +1,10 @@
 let vm = Vue.createApp({
     data() {
         return { isPurple: false };
+    },
+    computed: {
+        circle_classes() {
+            return { purple: this.isPurple };
+        }
     }
 }).mount('#app');
