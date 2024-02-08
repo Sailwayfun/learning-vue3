3599527d7a0c3263918fcdf5895a810a44b73dd1
Sailwayfun <liaoleon000513@gmail.com>
2024-02-08 08:59:03 +0800

    binding classes

--- section-2-18-STARTER/app.js
+++ section-2-18-STARTER/app.js
@@ -1,10 +1,14 @@
 let vm = Vue.createApp({
     data() {
-        return { isPurple: false };
+        return { isPurple: false, selectedColor: "white" };
     },
     computed: {
         circle_classes() {
-            return { purple: this.isPurple };
-        }
+            return {
+                purple: this.isPurple,
+                // ["text-orange"]: this.selectedColor === "text-orange",
+                // ["text-black"]: this.selectedColor === "text-black"
+            };
+        },
     }
 }).mount('#app');
