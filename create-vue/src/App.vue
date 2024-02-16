f883d61684374e885382942c66e621c8a794827b
Sailwayfun <liaoleon000513@gmail.com>
2024-02-16 22:39:00 +0800

    pass callback fn as props

--- create-vue/src/App.vue
+++ create-vue/src/App.vue
@@ -14,6 +14,9 @@ export default {
     addAgeBy3($event, parent_params) {
       this.age += $event
       this.age += parent_params
+    },
+    addAgeCB(num) {
+      this.age += num
     }
   }
 }
@@ -22,7 +25,12 @@ export default {
 <template>
   <p>Hey</p>
   <Greeting :age="age" />
-  <User :age="age" @increment-age="addAgeBy3($event, 1)" @decrement-age="age--" />
+  <User
+    :age="age"
+    @increment-age="addAgeBy3($event, 1)"
+    @decrement-age="age--"
+    :addAgeFn="addAgeCB"
+  />
   <!-- <button @click="age++">+</button>
   <button @click="age--">-</button> -->
 </template>
--- create-vue/src/components/User.vue
+++ create-vue/src/components/User.vue
@@ -1,13 +1,22 @@
 <template>
   <p>The User is {{ age }} years old</p>
   <button @click="increment">+</button>
+  <button @click="addAgeFn(3)">Add age Callback</button>
   <button @click="decrement">-</button>
 </template>
 
 <script>
 export default {
   name: 'user-user',
-  props: ['age'],
+  props: {
+    age: {
+      type: Number,
+      validator(value) {
+        return value > 0 && value < 130
+      }
+    },
+    addAgeFn: Function
+  },
   emits: ['increment-age', 'decrement-age'],
   methods: {
     increment() {
