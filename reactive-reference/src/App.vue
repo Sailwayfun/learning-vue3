e6e9e9fa5377ee7462a9cf63efbc749be13bfe48
Sailwayfun <liaoleon000513@gmail.com>
2024-02-19 22:22:34 +0800

    watch function

--- reactive-reference/src/App.vue
+++ reactive-reference/src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div>{{ number }}</div>
   <button @click="increment">Increment</button>
-  <button @click="incrementPrimitive">Increment Reactive Primitive</button>
+  <!-- <button @click="incrementPrimitive">Increment Reactive Primitive</button> -->
   <p>{{ name }}</p>
   <p>{{ age }}</p>
-  <p>{{ somePrimitive }}</p>
+  <!-- <p>{{ somePrimitive }}</p> -->
+  <label>Reverse String Generator</label>
+  <div>
+    <input type="text" v-model="phrase" />
+    <p>{{ reversedPhrase }}</p>
+  </div>
 </template>
 
 <script>
-import { ref, reactive, toRefs } from "vue";
+import { ref, reactive, toRefs, watch } from "vue";
 export default {
   name: "App",
   setup() {
@@ -27,18 +32,27 @@ export default {
       user.name = "Luis";
     }, 3000);
 
-    let somePrimitive = reactive(3);
+    // let somePrimitive = reactive(3);
 
-    function incrementPrimitive() {
-      somePrimitive++;
-    }
+    // function incrementPrimitive() {
+    //   somePrimitive++;
+    // }
+
+    let phrase = ref("");
+    let reversedPhrase = ref("");
+
+    watch(phrase, (newVal, oldVal) => {
+      reversedPhrase.value = [...phrase.value].reverse().join("");
+    });
 
     return {
       number,
       increment,
-      incrementPrimitive,
+      // incrementPrimitive,
       ...toRefs(user),
-      somePrimitive,
+      // somePrimitive,
+      phrase,
+      reversedPhrase,
     };
   },
 };
