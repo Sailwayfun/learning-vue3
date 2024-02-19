34045b8744bd7daaef09c5a61c9b3faea2ea2801
Sailwayfun <liaoleon000513@gmail.com>
2024-02-19 22:08:44 +0800

    reactive function

--- reactive-reference/src/App.vue
+++ reactive-reference/src/App.vue
@@ -1,20 +1,44 @@
 <template>
   <div>{{ number }}</div>
   <button @click="increment">Increment</button>
+  <button @click="incrementPrimitive">Increment Reactive Primitive</button>
+  <p>{{ name }}</p>
+  <p>{{ age }}</p>
+  <p>{{ somePrimitive }}</p>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, reactive, toRefs } from "vue";
 export default {
   name: "App",
   setup() {
     let number = ref(0);
+
     function increment() {
       number.value++;
     }
+
+    const user = reactive({
+      name: "John",
+      age: 20,
+    });
+
+    setTimeout(() => {
+      user.name = "Luis";
+    }, 3000);
+
+    let somePrimitive = reactive(3);
+
+    function incrementPrimitive() {
+      somePrimitive++;
+    }
+
     return {
       number,
       increment,
+      incrementPrimitive,
+      ...toRefs(user),
+      somePrimitive,
     };
   },
 };
