dfc601b877635c6af6dd7955d920494d5912d17c
Sailwayfun <liaoleon000513@gmail.com>
2024-02-19 21:29:02 +0800

    reactive reference

---
+++ mixins/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div class="scrollPosition">{{ offset }}</div>
+  </div>
+</template>
+
+<script>
+import offset from "./mixin";
+export default {
+  name: "App",
+  mixins: [offset],
+  data() {
+    return { offset: 0 };
+  },
+  mounted() {
+    console.log("app mounted run");
+  },
+};
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 3000px;
+  position: relative;
+}
+
+.scrollPosition {
+  position: fixed;
+  left: 10px;
+  right: 10px;
+}
+</style>
---
+++ reactive-reference/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{ number }}</div>
+  <button @click="increment">Increment</button>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "App",
+  setup() {
+    let number = ref(0);
+    function increment() {
+      number.value++;
+    }
+    return {
+      number,
+      increment,
+    };
+  },
+};
+</script>
