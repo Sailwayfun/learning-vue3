44d1ffc9597b3f2e9593788ee670c79eb1bb4f64
Sailwayfun <liaoleon000513@gmail.com>
2024-02-21 16:31:33 +0800

    emit events

---
+++ emit-events/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+import Increment from "./components/Increment.vue";
+let count = ref(0);
+function incrementCountBy10(num) {
+  count.value += num;
+}
+</script>
+
+<template>
+  <input type="text" v-model="count" />
+  <Increment @increment="incrementCountBy10" />
+</template>
+
+<style scoped></style>
---
+++ emit-events/src/components/Increment.vue
@@ -0,0 +1,13 @@
+<template>
+  <button @click="incrementCountBy10">+</button>
+</template>
+
+<script setup>
+const emit = defineEmits(["increment"]);
+
+function incrementCountBy10() {
+  emit("increment", 10);
+}
+</script>
+
+<style scoped></style>
