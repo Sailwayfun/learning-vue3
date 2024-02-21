f905a1b62a47a30ee0a17df63ea044e5dc790fc8
Sailwayfun <liaoleon000513@gmail.com>
2024-02-21 14:39:29 +0800

    template refs

---
+++ template-refs/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+let input = ref(null);
+onMounted(() => {
+  input.value.addEventListener("focus", changeBorderColor);
+});
+
+function changeBorderColor() {
+  input.value.style.border = "10px solid purple";
+}
+</script>
+
+<template>
+  <input ref="input" />
+</template>
+
+<style scoped></style>
